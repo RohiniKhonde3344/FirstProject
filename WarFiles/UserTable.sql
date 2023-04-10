912fbcd673f7139ffa75bad9575d2baa0994946f
vaibhavwadkar <vaibhav5509vw@gmail.com>
2023-04-10 18:29:48 +0530

    Added war file of spring boot application and war file and sql schema of servlet app

---
+++ WarFiles/UserTable.sql
@@ -0,0 +1,22 @@
+/*
+-- Query: SELECT * FROM firstdb.user
+LIMIT 0, 1000
+
+-- Date: 2023-04-10 17:07
+*/
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('abcd','123',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('Ankita','ak@123',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('Atharva','12345',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('me','you',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('pranjali','p@123',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('sahil','s@123',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('Tula','Tuch re',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('user1','pass@1',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('user2','pass@2',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('user3','pass@3',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('user4','pass@4',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('user5','pass@5',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('user6','pass@6',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('user7','pass@7',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('user8','pass@8',NULL);
+INSERT INTO `` (`username`,`password`,`useraddr`) VALUES ('vaibhav','vaibhav1',NULL);
